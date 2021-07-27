39ef79bece931c0ed5e2cd21cf4e53e7df246a8b
Andrew Pak <Andrew.Pak78@qmail.cuny.edu>
2021-07-27 14:47:36 -0400

    started graph class, I'll get back to this later

--- Project2/MSTOlympic.py
+++ Project2/MSTOlympic.py
@@ -1,1 +1,14 @@
+import sys # for INT_MAX
 print("hello minimum spanning tree")
+
+class Graph():
+
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)] for row in range(vertice)]
+
+    def printMST(self, parent):
+        print("Edge \t Weight")
+        for i in range(1, self.V):
+            print parent[i], "-", i, "\t", self.graph[i][parent[i]]
+
