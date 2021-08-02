ac446b11dd2303a9d235e8affc2631c444cbf388
BenKluger <bennykluger@gmail.com>
2021-08-02 16:56:29 -0400

    Got Kruskal's Algo working!

--- Project2/HeapThing.py
+++ Project2/HeapThing.py
@@ -1,6 +1,6 @@
 def makeAdjacencyMatrix(fileName):
 
-    INF = float('inf')
+    #INF = float('inf')
 
 
     vertices = 0
@@ -10,7 +10,7 @@ def makeAdjacencyMatrix(fileName):
 
     rows, cols = (vertices, vertices)
     graph = [[INF for i in range(cols)] for j in range(rows)]
-    print(graph)
+    #print(graph)
 
     #graph = [vertices][vertices] 
     with open(fileName) as f:
@@ -38,13 +38,73 @@ def makeAdjacencyMatrix(fileName):
             # to_vertex: '4', weight: '5'
             # to_vertex: '5', weight: '4'
             for to_vertex, weight in remaining_values:
-                print(from_vertex, to_vertex, weight)
+                #print(from_vertex, to_vertex, weight)
                 #Add to graph, replace INF (Convert to int() first)
                 fromV, toV = int(from_vertex), int(to_vertex)
                 if fromV >= vertices or toV >= vertices:
                     print("FromV or ToV is out of range", fromV, toV)
                     exit                    
-                graph[int(from_vertex)][int(to_vertex)] = weight
-    print(graph)
+                graph[int(from_vertex)][int(to_vertex)] = int(weight) #fixed this code so that weight is an int
+    
+    #print(graph)
+    return graph
 
+
+
+print("\nThis is Kruskal's Algorithm section\n")
+'''KRUSKALS ALGO SECTION'''
+
+# Python implementation for Kruskal's
+# algorithm
+ 
+# Find set of vertex i
+def find(i):
+    while parent[i] != i:
+        i = parent[i]
+    return i
+ 
+# Does union of i and j. It returns
+# false if i and j are already in same
+# set.
+def union(i, j):
+    a = find(i)
+    b = find(j)
+    parent[a] = b
+ 
+# Find MST using Kruskal's algorithm
+def kruskalMST(cost):
+    mincost = 0 # Cost of min MST
+ 
+    # Initialize sets of disjoint sets
+    for i in range(V):
+        parent[i] = i
+ 
+    # Include minimum weight edges one by one
+    edge_count = 0
+    while edge_count < V - 1:
+        min = INF
+        a = -1
+        b = -1
+        for i in range(V):
+            for j in range(V):
+                if find(i) != find(j) and cost[i][j] < min:
+                    min = cost[i][j]
+                    a = i
+                    b = j
+        union(a, b)
+        print('Edge {}:({}, {}) cost:{}'.format(edge_count, a, b, min))
+        edge_count += 1
+        mincost += min
+ 
+    print("Minimum cost= {}".format(mincost))
+
+
+'''DRIVER CODE FOR KRUSKALS'''
+V = 5
+parent = [i for i in range(V)]
+INF = float('inf')
 matrix = makeAdjacencyMatrix('real_input.txt')
+
+ 
+# Print the solution
+kruskalMST(matrix)
